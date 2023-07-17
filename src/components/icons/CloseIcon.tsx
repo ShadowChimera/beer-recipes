ca8c9c4357d02f3e6d5a49f44eec1e38c1fb3e2f
Alexey Babenko <alexey22042001@gmail.com>
2023-07-17 19:21:19 +0300

    Add CloseIcon

---
+++ src/components/icons/CloseIcon.tsx
@@ -0,0 +1,13 @@
+export const CloseIcon = () => {
+  return (
+    <svg
+      className="inline-block w-auto h-full"
+      xmlns="http://www.w3.org/2000/svg"
+      height={48}
+      viewBox="0 -960 960 960"
+      width={48}
+    >
+      <path d="M249-207l-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231z" />
+    </svg>
+  );
+};
